3d1f44f0f768e6bca90fb7074c733b3fd66ab0f8
Codi Kraus <codi.kraus@gmail.com>
2021-04-21 02:10:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def move(array, index, char = "X")
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
+  puts "Please enter 1-9: "
   user_input = gets.strip
   input_to_index(user_input)
   if valid_move(board, index) == true
