aea91ae61a4e10298c48d566c8f4c5ed7cda682a
Codi Kraus <codi.kraus@gmail.com>
2021-04-21 01:57:00 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,32 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && position_taken?(board, index) == false
+    return true
+  elsif position_taken?(board, index) == true
+    return false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " "
+    return false
+  elsif board[index] == ""
+    return false
+  elsif board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
+
+def input_to_index(user_input)
+  converted_input = user_input.to_i
+  index = converted_input - 1
+end
