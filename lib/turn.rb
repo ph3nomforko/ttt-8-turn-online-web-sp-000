83dfe4fbce91e608e1296766474371b59627834b
Codi Kraus <codi.kraus@gmail.com>
2021-04-21 02:19:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index) == true
-    move(board, index, char)
+    move(board, index, char = "X")
   else
     turn(board)
   end
